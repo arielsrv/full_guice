3531cc9b75e626b6398051dc6f44910496adb55b
ariel.pineiro <ariel.pineiro@iskaypet.com>
2025-07-25 15:52:20 +0200

    asdasd

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"awesomeProject19/di"
 	"awesomeProject19/services"
-	"fmt"
-	"os"
 )
 
 func main() {
@@ -14,13 +12,6 @@ func main() {
 	container.Provide(di.ProvideEmailWorker, di.Named("email_worker"))
 	container.Provide(di.ProvideSMSWorker, di.Named("sms_worker"))
 	container.Provide(services.NewNotificationService)
-
-	// Mostrar el grafo de dependencias
-	fmt.Println("Grafo de dependencias:")
-	if err := di.Visualize(container, os.Stdout); err != nil {
-		fmt.Printf("Error al visualizar el grafo: %v\n", err)
-	}
-	fmt.Println("\n---")
 
 	// Ejecutar
 	err := container.Invoke(func(service *services.NotificationService) {
